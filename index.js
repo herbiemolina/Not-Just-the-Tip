671da87841de092afac3f7751b5aa2d07972f8f5
herbiemolina <herbiemolina@gmail.com>
2019-06-06 21:15:20 -0400

    the user can split the check

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//import inquirer
 var inquirer = require("inquirer")
 
 
-
+//initialize inquirer
 inquirer
  .prompt([
      {
@@ -20,22 +21,53 @@ inquirer
         type: "confirm",
         message: "Would you like to split the check?",
         name: "split",
-
-        if (y) {
-            console.log("I'm a genius!")
-        }
      }
+
  ]).then(function(inquirerResponse) {
+//the user can split the check
+    if (inquirerResponse.split) {
+        // console.log("test")
+        inquirer
+        .prompt([
+            {
+                type: "number",
+                message: "How many people are in your party that are splitting the check?",
+                name:"party"
+            }
+        ]).then(function(partyCheck) {
 
-    var check = parseInt(inquirerResponse.check);
-    var percent = parseInt(inquirerResponse.tip) * .01;
-    var tip = check * percent;
-    var total = check + tip;
+            var party = parseInt(partyCheck.party);
+            // console.log(party)
+            var check = parseInt(inquirerResponse.check);
+            // console.log(check)
+            var percent = parseInt(inquirerResponse.tip) * .01;
+            var tip = check * percent;
+            var total = check + tip;
+            var splitCheck = total / party 
 
-    console.log("the tip is $" + tip);
+            console.log("The tip is: $" + tip);
+            console.log("the total is: $" + total);
+            console.log ("If you split the check evenly, each parton owes: $" + parseFloat(splitCheck).toFixed(2))
+        })
 
+    }
+    // the user can also choose not to split the check
+    else {
+        // console.log("test no")
 
-    console.log("the total is $" + total);
-    // console.log(inquirerResponse.check)
+        var check = parseInt(inquirerResponse.check);
+        var percent = parseInt(inquirerResponse.tip) * .01;
+        var tip = check * percent;
+        var total = check + tip;
+    
+        console.log("the tip is: $" + tip);
+    
+    
+        console.log("the total is: $" + total);
+        // console.log(inquirerResponse.check)
+
+    }
+
+   
 
  })
